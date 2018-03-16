aaeb1936eeab75c15f6330189b39e28856f2d85c
bmehler <bernhard.mehler@gmail.com>
2018-03-16 15:04:41 +0100

    WCAG Plugin - Formular neugeschrieben - Einbindung mit do_shortcode - Formular wird angezeigt - Fehlermeldungen funktionieren - Captcha funktioniert - Formular funktioniert Noch offen: - Formular versenden mit wp_mail - Benutzerinformationen ausgeben

--- includes/shortcode/wcag-contact-form-shortcode.php
+++ includes/shortcode/wcag-contact-form-shortcode.php
@@ -16,60 +16,60 @@ function show_contact_form( $atts ) {
 function create_form($email = '') {
     
     $salt = getSalt();
-    
     $captcha_array = getCaptcha();
-    
-    print_r($captcha_array['task_encrypted']);
+    $flag = 0;
+    $output = '';
     
     if(isset($_POST['submit'])) {
        
         $form_values = $_POST; 
         $errors = checkErrors($form_values);
-        
         $ans = $form_values['captcha'];
-        echo '<pre>';
-        print_r($ans);
-        echo '</pre>';
-      
         $checksum = md5($ans);
-      
-        echo '<pre>';
-        print_r($checksum);
-        echo '</pre>';
-        $saltet = md5($checksum.$salt);
-        echo '<pre>';
-        print_r($saltet);
-        echo '</pre>';
-        echo '<pre>';
-        print_r($form_values['ans']);
-        echo '</pre>';
+        $salted = md5($checksum.$salt);
+        
+        if(isset($errors) AND !count($errors)) {
+            if (in_array($salted, $form_values['ans'])) { 
+                echo '<h2>Vielen Dank! Wir werden uns umgehend bei Ihnen melden!</h2>';
+                $flag = 1;
+            } else {
+                $errors['newcaptcha'] = 'Falsches Captcha! Versuchen Sie es erneut.';
+                $flag = 0;
+            }
+        
+        }
        
     }
     
-    $output  =  '<h2>Probleme bei der Bedienung der Seite?</h2>';
-    $output .=  '<p>Sollten Sie Probleme bei der Bedingung der Webseite haben, füllen Sie bitte das Feedback-Formular aus!</p><br />';
-    $output .=  '<form method="post" id="captchaform">';
-    $encry = md5($captcha_array['task_encrypted'].$salt);
-    $output .=  '<p><input type="hidden" name="ans[]" value="' . $encry . '"/></p>';
-    $output .=  (isset($errors['feedback']) ? '<div style="color:red;">' . $errors['feedback'] . '</div>' : '');
-    $output .=  '<p><label for="feedback">Ihr Feedback</label>';
-    $output .=  '<textarea id="feeadback" name="feedback" cols="150" rows="10">' . (isset($_POST['submit']) ? htmlspecialchars($form_values['feedback'], ENT_QUOTES) : '') .'</textarea></p>';
-    //$output .=  '<textarea id="feeadback" name="feedback" cols="150" rows="10"></textarea></p>';
-    $output .=  (isset($errors['captcha']) ? '<div style="color:red;">' . $errors['captcha'] . '</div>' : '');
-    $output .=  (isset($errors['tomany']) ? '<div style="color:red;">' . $errors['tomany'] . '</div>' : '');
-    $output .=  '<p><label for="check">Lösen Sie folgende Aufgabe:</label></p>';
-    $output .=  '<p>' . $captcha_array['task_string'] . ' ' . '<input type="text" name="captcha" id="check"/></p>';
-    $output .=  '<input type="submit" name="submit" form="captchaform" value="Senden">';
-    $output .=  '</form>';
-    $output .=  '</div>';
+    if($flag == 0) {
+        $output  =  '<h2>Probleme bei der Bedienung der Seite?</h2>';
+        $output .=  '<p>Sollten Sie Probleme bei der Bedingung der Webseite haben, füllen Sie bitte das Feedback-Formular aus!</p><br />';
+        $output .=  '<form method="post" id="captchaform">';
+        $output .=  (isset($errors['name']) ? '<div style="color:red;">' . $errors['name'] . '</div>' : '');
+        $output .=  '<p><label for="name">Name:</label></p><p><input type="text" name="name" id="name" value="' . (isset($_POST['submit']) ? htmlspecialchars($form_values['name'], ENT_QUOTES) : '') . '"/></p>';
+        $output .=  (isset($errors['email']) ? '<div style="color:red;">' . $errors['email'] . '</div>' : '');
+        $output .=  (isset($errors['validaddress']) ? '<div style="color:red;">' . $errors['validaddress'] . '</div>' : '');
+        $output .=  '<p><label for="email">E-Mail:</label></p><p><input type="text" name="email" id="email" value="' . (isset($_POST['submit']) ? htmlspecialchars($form_values['email'], ENT_QUOTES) : '') . '"/></p>';
+        $encry = md5($captcha_array['task_encrypted'].$salt);
+        $output .=  '<p><input type="hidden" name="ans[]" value="' . $encry . '"/></p>';
+        $output .=  (isset($errors['feedback']) ? '<div style="color:red;">' . $errors['feedback'] . '</div>' : '');
+        $output .=  '<p><label for="feedback">Ihr Feedback</label>';
+        $output .=  '<textarea id="feeadback" name="feedback" cols="150" rows="10">' . (isset($_POST['submit']) ? htmlspecialchars($form_values['feedback'], ENT_QUOTES) : '') .'</textarea></p>';
+        //$output .=  '<textarea id="feeadback" name="feedback" cols="150" rows="10"></textarea></p>';
+        $output .=  (isset($errors['captcha']) ? '<div style="color:red;">' . $errors['captcha'] . '</div>' : '');
+        $output .=  (isset($errors['validcaptcha']) ? '<div style="color:red;">' . $errors['validcaptcha'] . '</div>' : '');
+        $output .=  (isset($errors['newcaptcha']) ? '<div style="color:red;">' . $errors['newcaptcha'] . '</div>' : '');
+        $output .=  '<p><label for="check">Lösen Sie folgende Aufgabe:</label></p>';
+        $output .=  '<p>' . $captcha_array['task_string'] . ' ' . '<input type="text" name="captcha" id="check"/></p>';
+        $output .=  '<input type="submit" name="submit" form="captchaform" value="Senden">';
+        $output .=  '</form>';
+        $output .=  '</div>';
+    }
                                    
-  echo $output;                           
-    
-}   
+  echo $output;  
+}
 
 function checkErrors($values) {
-    
-    print_r($values);
     
     $hasErrors = array();
     
@@ -84,9 +84,22 @@ function checkErrors($values) {
         if($_POST['captcha'] =='') {
             $hasErrors['captcha'] = "Bitte geben Sie das Captcha ein.";
         }
-        /*if(!preg_match($pattern, $_POST['captcha'])) {
-            $hasErrors['tomany'] = "Sie können nur eine Ziffer eingeben";
-        }*/
+        if($_POST['name'] =='') {
+            $hasErrors['name'] = "Bitte geben Sie Ihren Namen ein.";
+        }
+        if($_POST['email'] =='') {
+            $hasErrors['email'] = "Bitte geben Sie Ihre E-Mail ein.";
+        }
+        if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            if(!empty($_POST['email'])) {
+                $hasErrors['validaddress'] = "Bitte geben Sie das richtige Format ein. (name@domain.de)";
+            }
+        }
+        if(!preg_match('/^[0-9]{1,2}$/', $_POST['captcha'])) {
+            if(!empty($_POST['captcha'])) {
+                $hasErrors['validcaptcha'] = "Sie können maximal zwei Ziffern eingeben";
+            }
+        }
     }
     
     return $hasErrors;
