6c97e74271bff69b9ff709ed5aac0ea68d7b4c0c
bmehler <bernhard.mehler@gmail.com>
2018-04-27 17:23:07 +0200

    - Widget im Dashboard angelegt

--- includes/menu/wcag-add-footer-menu.php
+++ includes/menu/wcag-add-footer-menu.php
@@ -3,37 +3,62 @@
 namespace RRZE\Wcag;
 
 function wcag_dashboard_widget_function( $post, $callback_args ) {
-	echo "Das <strong>WCAG Plugin</strong> kann einen Menueintrag im Menü-Footer nicht anlegen. Wahrscheinlich haben Sie bereits einen Menüeintrag mit dem Namen <strong>Barrierefreiheit</strong>. Ändern Sie bitte den Namen Ihrer Seite.";
+	echo "Das <strong>WCAG Plugin</strong> kann keinen Menüeintrag in der <strong>Navigation unten</strong> anlegen. Wahrscheinlich haben Sie bereits einen Menüeintrag mit dem Namen <strong>Barrierefreiheit</strong> angelegt. Ändern Sie bitte den Namen Ihrer Seite.";
 }
 
 function wcag_add_dashboard_widgets() {
 	wp_add_dashboard_widget('wcag_dashboard_widget', '<span class="dashicons dashicons-awards"></span>Achtung!', 'RRZE\Wcag\wcag_dashboard_widget_function');
 }
 
+function wcag_dashboard_widget_menu_function( $post, $callback_args ) {
+	echo "Das <strong>WCAG Plugin</strong> kann keinen Menüeintrag in der Navigation unten anlegen. Bitte legen Sie ein Footer-Menü an!";
+}
+
+function wcag_add_dashboard_menu_widgets() {
+	wp_add_dashboard_widget('wcag_dashboard_widget', '<span class="dashicons dashicons-awards"></span>Achtung!', 'RRZE\Wcag\wcag_dashboard_widget_menu_function');
+}
+
 function example_remove_dashboard_widget() {
- 	remove_meta_box( 'wcag_dashboard_widget', 'dashboard', 'side' );
+ 	remove_meta_box( 'wcag_dashboard_widget', 'dashboard', 'normal' );
 } 
+
+function the_slug_exists($page_slug) {
+
+     $page = get_page_by_path( $page_slug , OBJECT );
+
+     if ( isset($page) )
+        return true;
+     else
+        return false;
+}
  
 function add_page_to_footer_menu() {
     
     $current_theme = wp_get_theme();
     $themes_fau = array('FAU-Einrichtungen', 'FAU-Natfak', 'FAU-Philfak', 'FAU-RWFak', 'FAU-Techfak', 'FAU-Medfak');
+    $menu_entry_option = 'wcag_menu';
+    
+    if (the_slug_exists('barrierefreiheit')) {
+        error_log(print_r('berni', true));
+        $slug_exists = true;
+    }
     
     if(in_array($current_theme, $themes_fau)) {
         if(has_nav_menu('meta-footer')) {
             $menu_name = 'meta-footer';
             $menu_id = wp_get_nav_menu_object( get_nav_menu_locations( $menu_name )[ $menu_name ] )->term_id;
-           
             $menu_items = wp_get_nav_menu_items($menu_id);
-            #error_log(print_r($menu_items, true));
+            error_log(print_r($menu_items, true));
             foreach($menu_items as $menu_item) {
                 $title = $menu_item->title;
                 if($title == 'Barrierefreiheit') {
+                    if($menu_item->object != 'custom') {
+                    #if($slug_exists) {
+                        add_action('wp_dashboard_setup', 'RRZE\Wcag\wcag_add_dashboard_widgets' );
+                    }
                     $used = true;
-                    add_action('wp_dashboard_setup', 'RRZE\Wcag\wcag_add_dashboard_widgets' );
                 } else {
                     $used = false;
-                    #add_action('wp_dashboard_setup', 'example_remove_dashboard_widget' );
                 }
             }
     
@@ -45,6 +70,8 @@ function add_page_to_footer_menu() {
                     'menu-item-status' => 'publish')
                 );
             }
+        } else {
+            add_action('wp_dashboard_setup', 'RRZE\Wcag\wcag_add_dashboard_menu_widgets' );
         }
     }  
 }
