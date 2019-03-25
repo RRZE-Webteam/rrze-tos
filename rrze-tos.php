0089852347d40356460a5449665ec13e55998ff0
Rolf <rolf.v.d.forst@fau.de>
2019-03-25 15:49:06 +0100

    Update plugin version

--- rrze-tos.php
+++ rrze-tos.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE TOS
 Plugin URI:      https://github.com/RRZE-Webteam/rrze-tos
 Description:     Prüfung einer Website aus dem FAU-Netzwerk gemäß den Konformitätskriterien der TOS.
-Version:         1.3.0
+Version:         1.3.1
 Author:          RRZE-Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v2
